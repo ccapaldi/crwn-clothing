c135c356443bbab95802c458903d5c08eb927f9a
Carey Capaldi <cckfwiz@gmail.com>
2020-05-05 07:59:34 -0700

    added redux, and implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Switch, Route } from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
@@ -7,41 +8,37 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import {setCurrentUser} from './redux/user/user.actions';
 
 //import { ReactComponent } from '../src/assets/original.svg';
 
 class  App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
+  
 unsubscribeFromAuth = null
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
+
    this.unsubscribeFromAuth =  auth.onAuthStateChanged(async userAuth => {
       //this.setState({currentUser: user});
-      console.log("Firing user profile");
+     // console.log("Firing user profile");
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot( snapShot => {
           //console.log(snapShot.data());
-          this.setState({
-            currentUser: {
+         setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
-          })
-          console.log(this.state);
-        });
+            });
+          });
+          //console.log(this.state);
+        }
         
-        this.setState({currentUser: userAuth});
-      } 
+        setCurrentUser(userAuth);
+      });
        
-    })
+    //})
   
   }
 
@@ -53,7 +50,7 @@ unsubscribeFromAuth = null
   render() {
   return (
     <div>
-     <Header currentUser={this.state.currentUser} />
+     <Header  />
       <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
@@ -63,5 +60,7 @@ unsubscribeFromAuth = null
     </div>
   );}
 }
-
-export default App;
+ const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+ });
+export default connect(null, mapDispatchToProps) (App);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -51,7 +51,7 @@ handleChange = event => {
             <CustomButton type="submit" >
             Sign in
             </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn >
+            <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn >
             {' '}
             Sign in with Google {' '}
             </CustomButton>
