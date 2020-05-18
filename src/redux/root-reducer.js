3d1bbadbcab13b1b893757cbb4a9cac8539803f7
Carey Capaldi <cckfwiz@gmail.com>
2020-05-18 08:58:53 -0700

    put directory into it's own reducer and updated the directory component to call it

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -6,6 +6,7 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from '../redux/directory/directory.reducer';
 
 const persistConfig = {
     key: 'root',
@@ -17,6 +18,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers ({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
+    
 });
 export default persistReducer(persistConfig, rootReducer);
